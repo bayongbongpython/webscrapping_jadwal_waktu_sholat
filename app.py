263005c10dcb051cafbcad725135d6e0f45590ba
WORK <andi.hidayat.mail@gmail.com>
2020-11-22 13:23:20 +0700

    scrapping data pkpu

--- app.py
+++ app.py
@@ -6,5 +6,24 @@ contents = requests.get(url)
 
 response = bs4.BeautifulSoup(contents.text, "html.parser")
 data = response.find_all('tr', 'table_highlight')
-print(data[0])
+data = data[0]
+
+sholat = {}
+i = 0
+for d in data:
+    if i == 1:
+        sholat['subuh'] = d.get_text()
+    elif i == 2:
+        sholat['zuhur'] = d.get_text()
+    elif i == 3:
+        sholat['ashar'] = d.get_text()
+    elif i == 4:
+        sholat['magrib'] = d.get_text()
+    elif i == 5:
+        sholat['magrib'] = d.get_text()
+    elif i == 6:
+        sholat['isya'] = d.get_text()
+    i += 1
+
+print(sholat)
 
